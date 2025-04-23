5e6da41078167223b445926ede5558a010d07ccc
daniellazureanu <wqj831344@gmail.com>
2025-04-23 04:12:00 +0300

    bug fixes

--- src/kanacli.cpp
+++ src/kanacli.cpp
@@ -9,24 +9,43 @@
 #include <utility>
 #include <algorithm>
 #include <random>
+#include <unistd.h>
+
+struct ColorRGB {
+	int r, g, b;
+};
+
+// COLOR PALETTE
+// https://coolors.co/palette/cdb4db-ffc8dd-ffafcc-bde0fe-a2d2ff
+const ColorRGB THISTLE = {205, 180, 219};
+const ColorRGB FAIRY_TALE = {255, 200, 221};
+const ColorRGB CARNATION_PINK = {255, 175, 204};
+const ColorRGB URANIAN_BLUE = {189, 224, 254};
+const ColorRGB LIGHT_SKY_BLUE = {162, 210, 255};
 
 void moveCursor(int x, int y);
 void clearScreen();
 void changeColor(int colorType, int r, int g, int b);
+void changeColor(int colorType, const ColorRGB &color);
 void changeColor(int colorType);
 void loadFile(std::string filePath, std::ifstream &inputFile);
 void populateDictionary(std::ifstream &inputFile, std::vector<std::pair<std::string, std::string>> &kanaDictionary);
 void shuffleKana(std::vector<std::pair<std::string, std::string>> &kanaDictionary);
+void printFeedback(int correctAnswers, int wrongAnswers);
 void printInterface(std::ifstream &inputFile);
 void printKana(int x, int y, std::vector<std::pair<std::string, std::string>> &kanaDictionary, size_t kanaIndex);
-void checkInput(std::vector<std::pair<std::string, std::string>> kanaDictionary, size_t &kanaIndex);
+void checkInput(std::vector<std::pair<std::string, std::string>> kanaDictionary, size_t &kanaIndex, int &correctAnswers, int &wrongAnswers);
 
 int main(){
+	int correctAnswers = 0;
+	int wrongAnswers = 0;
+	//int totalKana = 8;
+	
 	std::vector<std::pair<std::string, std::string>> kanaDictionary;
 	std::ifstream inputFile;
 	
 	clearScreen();
-	
+
 	loadFile("./assets/hiragana.kana", inputFile);
 	populateDictionary(inputFile, kanaDictionary);
 	inputFile.close();
@@ -36,12 +55,14 @@ int main(){
 	loadFile("./assets/interface.kana", inputFile);
 	printInterface(inputFile);
 	inputFile.close();
+	printFeedback(correctAnswers, wrongAnswers);
 	
-	size_t kanaIndex = kanaDictionary.size()-1;
+	size_t kanaIndex = 0;
 	
-	while(kanaIndex > 0){
+	while(kanaIndex < kanaDictionary.size()){
 		printKana(3, 22, kanaDictionary, kanaIndex);
-		checkInput(kanaDictionary, kanaIndex);
+		checkInput(kanaDictionary, kanaIndex, correctAnswers, wrongAnswers);
+		printFeedback(correctAnswers, wrongAnswers);
 	}
 
 	changeColor(RESET);
@@ -61,6 +82,14 @@ void changeColor(int colorType, int r, int g, int b) {
 	else if(colorType==BACKGROUND){
 		std::cout << "\033[48;2;" << r << ";" << g << ";" << b << "m";
 	}
+}
+
+void changeColor(int colorType, const ColorRGB &color){
+	if(colorType==FOREGROUND)
+		std::cout << "\033[38;2;" << color.r << ";" << color.g << ";" << color.b << "m";
+	else if(colorType==BACKGROUND){
+		std::cout << "\033[48;2;" << color.r << ";" << color.g << ";" << color.b << "m";
+	}	
 }
 
 void changeColor(int colorType) {
@@ -105,10 +134,23 @@ void shuffleKana(std::vector<std::pair<std::string, std::string>> &kanaDictionar
 	std::default_random_engine rng(rd());
 	std::shuffle(kanaDictionary.begin(), kanaDictionary.end(), rng);	
 }
+
+void printFeedback(int correctAnswers, int wrongAnswers){
+	moveCursor(7,66);
+	changeColor(FOREGROUND, URANIAN_BLUE);
+	std::cout<<"Correct: " << correctAnswers;
+	
+	moveCursor(8, 66);
+	changeColor(FOREGROUND, CARNATION_PINK);
+	std::cout<<"Wrong:   " << wrongAnswers;
+	
+	changeColor(RESET);
+}
+
 void printInterface(std::ifstream &inputFile){
 	std::string line;
 
-	changeColor(FOREGROUND, 255, 182, 193);
+	changeColor(FOREGROUND, THISTLE);
 	
 	while (getline(inputFile, line)) {
 		std::cout<<line<<std::endl;
@@ -124,7 +166,8 @@ void printKana(int x, int y, std::vector<std::pair<std::string, std::string>> &k
 	size_t end;
 	int lineOffset = 0;
 	
-	changeColor(FOREGROUND, 119, 221, 119);
+	changeColor(FOREGROUND, LIGHT_SKY_BLUE);
+	
 	while ((end = kanaArt.find('\n', start)) != std::string::npos) {
 		std::string line = kanaArt.substr(start, end - start);
 		moveCursor(x + lineOffset, y);
@@ -135,22 +178,22 @@ void printKana(int x, int y, std::vector<std::pair<std::string, std::string>> &k
 	changeColor(RESET);
 }
 
-void checkInput(std::vector<std::pair<std::string, std::string>> kanaDictionary, size_t &kanaIndex){
+void checkInput(std::vector<std::pair<std::string, std::string>> kanaDictionary, size_t &kanaIndex, int &correctAnswers, int &wrongAnswers){
 	const auto& [romaji, kanaArt] = kanaDictionary[kanaIndex];
 	std::string guess;
 	
 	moveCursor(23, 37);
 	std::cout<<"   ";
 	moveCursor(23, 37);	
+	
 	std::cin>>guess;
 	
-	if(guess != romaji){
-		moveCursor(25,36);
-		std::cout<<"\033[31mWrong!\033[0m";
+	if(guess != romaji ){
+		wrongAnswers++;
+		kanaIndex++;
 	}
 	else{
-			moveCursor(25, 36);
-			std::cout<<"      ";
-			kanaIndex--;
+		correctAnswers++;
+		kanaIndex++;
 	}
 }
